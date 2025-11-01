7f8462cb8aad0079b98714b2c48e9a80519ea4b0
Mohamed Mahmoud Hasan <engmedo.9498@gmail.com>
2025-11-01 16:01:26 +0200

    upload project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    strings: ['Designer', 'Developer'],
+    typeSpeed: 60,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    loop: true,
+
+});
